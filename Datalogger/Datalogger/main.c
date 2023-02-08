f38db570a49145aab5b34eb8348cf1db10f5ab98
milocoder <milo.dewit@hotmail.com>
2023-02-08 14:17:59 +0100

    Initialisatie

---
+++ Datalogger/Datalogger/main.c
@@ -0,0 +1,18 @@
+/*
+ * Datalogger.c
+ *
+ * Created: 08/02/2023 14:15:31
+ * Author : School
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+

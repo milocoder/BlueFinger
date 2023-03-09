20ae50fbb067698c4f8bb39b700e7283d098169f
milocoder <milo.dewit@hotmail.com>
2023-03-09 13:42:00 +0100

    Update
    Dit gaat in de ijskast want de schakeling zelf klopt niet

--- Datalogger/Arduino/verstuur_can/verstuur_can.ino
+++ Datalogger/Arduino/verstuur_can/verstuur_can.ino
@@ -16,29 +16,24 @@ void setup() {
   SPI.begin();               
  
   mcp2515.reset();
-  mcp2515.setBitrate(CAN_500KBPS, MCP_8MHZ); // zet misschien mcp op 16 mhz
+  mcp2515.setBitrate(CAN_125KBPS, MCP_8MHZ); // zet misschien mcp op 16 mhz
   mcp2515.setNormalMode();  
   Serial.print("SPI and MCP ready"); 
 
 }
 
 void loop() {
-    buttonState = digitalRead(buttonPin);
     
-    if(buttonState == HIGH)
-    {
-      canMsg.can_id  = 0x036; // in dec is dit 54
-      canMsg.can_dlc = 8;               //CAN data length as 8
-      canMsg.data[0] = 1;               
-      canMsg.data[1] = 2;            
-      canMsg.data[2] = 3;            
-      canMsg.data[3] = 4;
-      canMsg.data[4] = 5;
-      canMsg.data[5] = 6;
-      canMsg.data[6] = 7;
-      canMsg.data[7] = 8;
-    
-      mcp2515.sendMessage(&canMsg);     //Sends the CAN message
-      delay(500);
-    }
+    canMsg.can_id  = 0x036; // in dec is dit 54
+    canMsg.can_dlc = 8;               //CAN data length as 8
+    canMsg.data[0] = 1;               
+    canMsg.data[1] = 2;            
+    canMsg.data[2] = 3;            
+    canMsg.data[3] = 4;
+    canMsg.data[4] = 5;
+    canMsg.data[5] = 6;
+    canMsg.data[6] = 7;
+    canMsg.data[7] = 8;
+  
+    mcp2515.sendMessage(&canMsg);     //Sends the CAN message
 }

7d5906122770a53757aa459cefee4bb15ed94552
JohanHogendoorn <johanhogendoorn2004@gmail.com>
2023-03-17 15:35:36 +0100

    asdfad

--- Hall_sensor/Hall_sensor/main.c
+++ Hall_sensor/Hall_sensor/main.c
@@ -16,7 +16,7 @@
 #include "millis.h"
 
 
-void writeFloatToEEPROM(float value, int address);
+void writeFloatToEEPROM(uint8_t value, int address);
 
 
 int main(void)
@@ -46,15 +46,11 @@ int main(void)
 			huidige_tijd_ms = millis();
 			unsigned long verschil_tijd_ms = huidige_tijd_ms - vorige_tijd_ms; 
 			unsigned long verschil_tijd_s = verschil_tijd_ms / 1000; 
-			
-			
-			//float tijd_seconden = (float)verschil_tijd_ms / 1000.0; 
-			//float snelheid_ms = OMTREK_WIEL * tijd_seconden; 	
-			
+	
 					
 			
-			float snelheidms = OMTREK_WIEL / verschil_tijd_s;
-			float snelheidKmH = snelheidms * 3.6;  		
+			uint8_t snelheidms = OMTREK_WIEL / verschil_tijd_s;
+			uint8_t snelheidKmH = snelheidms * 3.6;  		
 			writeFloatToEEPROM(snelheidKmH, addressHall);
 			addressHall += 3;		
 			
@@ -70,11 +66,11 @@ int main(void)
 }
 
 
-void writeFloatToEEPROM(float value, int address)
+void writeFloatToEEPROM(uint8_t value, int address)
 {
-	int val1 = (int)value; // pak getal voor de komma
-	int val2 = (int)((value-val1)*100)+1; // pak kommagetal en doe keer 100
+	uint8_t val1 = value; // pak getal voor de komma
+	//int val2 = (int)((value-val1)*100)+1; // pak kommagetal en doe keer 100
 	eeprom_write_byte((uint8_t*)address, val1);
-	eeprom_write_byte((uint8_t*)address+1, val2);
+	//eeprom_write_byte((uint8_t*)address+1, val2);
 	
 }

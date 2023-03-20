542050356e27f3706f6fdc0424ea967ed71f59b4
milocoder <milo.dewit@hotmail.com>
2023-03-20 11:21:57 +0100

    Last update

--- Hall_sensor/Hall_sensor/main.c
+++ Hall_sensor/Hall_sensor/main.c
@@ -1,12 +1,4 @@
-/*
- * Hall_sensor2.c
- *
- * Created: 17-3-2023 14:11:32
- * Author : johan
- */ 
-
 #define F_CPU 12000000UL
-
 
 #include <avr/io.h>
 #include <avr/interrupt.h>
@@ -34,82 +26,42 @@ int main(void)
 	unsigned long VorigeAantalMili = 0; 	
 	
 	int addressHall = 0;
-	float omtrek_wiel = 1.35; 
+	float omtrek_wiel = 1.35;
+	float TijdVerschilSeconden = 0; 
 		
 	DDRF = 0xFF;		//output ledje
 	DDRC = 0;		//input hall sensor
 	PORTC = 0;
 	
- 	
 	while(1) {
 	
 		unsigned long HuidigeAantalMili = millis();
-		if (!(~PINC & (1 << PC0))) {
-			rpmaantal++;
+		TijdVerschilSeconden = (float)(HuidigeAantalMili - VorigeAantalMili)/1000;
+		
+		if(TijdVerschilSeconden > 0.1)
+		{
+			if (!(~PINC & (1 << PC0))) {
+				rpmaantal++;
+			}			
 		}
-
-		// Meet de snelheid alleen als er minstens één omwenteling is gedetecteerd en een kwart seconde voorbij is
+		
+		// Meet de snelheid alleen als er minstens één omwenteling is gedetecteerd en 0.1 seconde voorbij is
 		if(rpmaantal > 0) {
-			
-			float TijdVerschilSeconden = (float)(HuidigeAantalMili - VorigeAantalMili)/1000;
-			
-			if(TijdVerschilSeconden > 0.5) {
+			PORTF = 0xFF;	//led lichtje voor feedback
 			snelheidms = omtrek_wiel / TijdVerschilSeconden; 
 			snelheidKmH = snelheidms * 3.6;
 			
 			writeFloatToEEPROM(snelheidKmH, addressHall);
 			
 			addressHall += 2;
-			PORTF = 0xFF;	//led lichtje voor feedback
-			
 
 			// Reset de teller en de timer voor de volgende meting
 			rpmaantal = 0;
-			VorigeAantalMili = HuidigeAantalMili;
-			}
-		
-		} else {
+			VorigeAantalMili = millis();
 			PORTF = 0x00;
-		}
-		
-		
-		
-		
+		}		
 	}
 }
-
-		
-		
-			
-			/*
-			if (!(~PINC & (1 << PC0))) 	{
-		
-				huidige_tijd_ms = millis();			
-				uint32_t verschil_tijd_ms = huidige_tijd_ms - vorige_tijd_ms; 
-				uint32_t verschil_tijd_s = verschil_tijd_ms / 1000; 
-				
-			
-				double snelheidms = omtrek_wiel / verschil_tijd_s;
-				double snelheidKmH = snelheidms * 3.6;  		
-				writeFloatToEEPROM(snelheidKmH, addressHall);
-				
-				addressHall += 2;	
-				PORTF = 0xFF;			
-			
-			
-				
-				vorige_tijd_ms = huidige_tijd_ms; 
-
-			} else {
-				PORTF = 0x00; 
-			}
-			
-		//vorige_status_hall = huidige_status_hall; 
-		
-	}
-}
-*/
-
 
 void writeFloatToEEPROM(float value, int address)
 {
--- WriteToEEPROM/WriteToEEPROM/main.c
+++ WriteToEEPROM/WriteToEEPROM/main.c
@@ -8,20 +8,8 @@ void writeFloatToEEPROM(float value, int address);
 
 int main(void)
 {
-	float speed1 = 12.78;
-	float speed2 = 23.65;
-	int address = 1;
-	writeFloatToEEPROM(speed1, address);
-	address += 3;
-	writeFloatToEEPROM(speed2, address);
-	// eeprom_write_byte((uint8_t*)12, 255);
-}
-
-void writeFloatToEEPROM(float value, int address)
-{
-	int val1 = (int)value; // pak getal voor de komma
-	int val2 = (int)((value-val1)*100)+1; // pak kommagetal en doe keer 100
-	eeprom_write_byte((uint8_t*)address, val1);
-	eeprom_write_byte((uint8_t*)address+1, val2);
-	
+	for (int i = 0; i<2000; i++)
+	{
+		eeprom_write_byte((uint8_t*)i, 0xFF);
+	}
 }
